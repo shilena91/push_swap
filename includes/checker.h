9f85a2cd8b738fc1db964558fb6f7fce982d062c
Hoang Pham <pham.hoang1591@gmail.com>
2020-01-08 10:21:43 +0200

    nothing done yet

---
+++ includes/checker.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: HoangPham <HoangPham@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/08 07:12:02 by HoangPham         #+#    #+#             */
+/*   Updated: 2020/01/08 07:13:54 by HoangPham        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CHECKER_H
+# define CHECKER_H
+
+# include "libft.h"
+# include "ft_printf.h"
+# include "get_next_line.h"
+
+#endif
